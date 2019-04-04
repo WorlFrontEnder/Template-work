e4a19ddf2a3eb84056512a79d4854693d46a6c80
Cyberverstalshik <Cyberverstalshik@yandex.ru>
2019-04-04 11:45:06 +0300

    Init multi slider

---
+++ build/js/multiple.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ /* Multiple Items */
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: "<img src='images/icon13.png' class='prev' alt='1'>",
+    nextArrow: "<img src='images/icon14.png' class='next' alt='2'>"
+  });
+
+}); /* End js code */
---
+++ dist/js/multi-vertical.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ /* Multiple Items */
+ $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    prevArrow: "<img src='images/icon13.png' class='prev' alt='1'>",
+    nextArrow: "<img src='images/icon14.png' class='next' alt='2'>"
+  });
+
+}); /* End js code */
